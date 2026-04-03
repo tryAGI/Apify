acd3d6fbdc333050c1ff27107f19c2cd74587969
Konstantin S. <havendv@gmail.com>
2026-04-03 09:01:30 +0400

    feat: Updated OpenAPI spec (#9)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Apify/Generated/Apify.LogsClient.LogGet.g.cs
+++ src/libs/Apify/Generated/Apify.LogsClient.LogGet.g.cs
@@ -388,7 +388,6 @@ namespace Apify
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
